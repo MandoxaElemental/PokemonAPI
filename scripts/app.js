0b44fd177f3ad1fd02c93850226d45389b15c425
Gianpaolo Reinares <gprcards@gmail.com>
2025-02-07 12:05:02 -0800

    made a public folder and fixed the mute button

--- scripts/app.js
+++ scripts/app.js
@@ -51,8 +51,9 @@ let X = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="cu
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 </svg>`;
+let PokemonAudioBool = true;
 
-document.getElementById("bgMusic").src = "/assets/audio/Pokémon TCG Pocket OST - Feed Menu BGM [ ezmp3.cc ].mp3";
+document.getElementById("bgMusic").src = "public/assets/audio/Pokémon TCG Pocket OST - Feed Menu BGM [ ezmp3.cc ].mp3";
 let Varieties = []
 
 let CurrentMon = "";
@@ -251,11 +252,11 @@ let MissingNoLocation = [
   "Time Capsule Exploit",
 ];
 let MissingNoFamily = [
-  "/assets/Missingno_RB.png",
-  "/assets/Missingno_Y.png",
-  "/assets/Spr_1b_142_f.png",
-  "/assets/Spr_1b_141_f.png",
-  "/assets/Ghost_I.png",
+  "public/assets/Missingno_RB.png",
+  "public/assets/Missingno_Y.png",
+  "public/assets/Spr_1b_142_f.png",
+  "public/assets/Spr_1b_141_f.png",
+  "public/assets/Ghost_I.png",
 ];
 
 let Start = document.getElementById('start')
@@ -268,7 +269,7 @@ Home.addEventListener("click", function () {
   Music();
   RandomPokemon();
   document.getElementById("bgMusic").loop = true;
-  document.getElementById("shinyAudio").src = "/assets/audio/Gen 9 Shiny Sparkle Sound Effect - Pokémon Scarlet and Violet [ ezmp3.cc ].mp3";
+  document.getElementById("shinyAudio").src = "public/assets/audio/Gen 9 Shiny Sparkle Sound Effect - Pokémon Scarlet and Violet [ ezmp3.cc ].mp3";
 });
 
 BGMusic.loop = true;
@@ -281,12 +282,14 @@ Mute.addEventListener("click", async () => {
   if (MuteBool === true) {
     BGMusic.pause();
     BGMusic.currentTime = 0;
-    document.getElementById("audioOn").src = "assets/volume-mute-fill.svg";
+    document.getElementById("audioOn").src = "/public/assets/volume-mute-fill.svg";
     MuteBool = false;
+    PokemonAudioBool = false;
   } else if (MuteBool === false) {
     Music();
     MuteBool = true;
-    document.getElementById("audioOn").src = "assets/volume-down-fill.svg";
+    document.getElementById("audioOn").src = "/public/assets/volume-down-fill.svg";
+    PokemonAudioBool = true;
   }
 });
 
@@ -302,58 +305,58 @@ function ToUpper(input) {
 function Types1() {
   switch (FirstType) {
     case "normal":
-      document.getElementById("type").src = "/assets/pokemonTypes/Normal.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Normal.png";
       break;
     case "fighting":
-      document.getElementById("type").src = "/assets/pokemonTypes/Fighting.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Fighting.png";
       break;
     case "flying":
-      document.getElementById("type").src = "/assets/pokemonTypes/Flying.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Flying.png";
       break;
     case "poison":
-      document.getElementById("type").src = "/assets/pokemonTypes/Poison.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Poison.png";
       break;
     case "ground":
-      document.getElementById("type").src = "/assets/pokemonTypes/Ground.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Ground.png";
       break;
     case "rock":
-      document.getElementById("type").src = "/assets/pokemonTypes/Rock.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Rock.png";
       break;
     case "bug":
-      document.getElementById("type").src = "/assets/pokemonTypes/Bug.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Bug.png";
       break;
     case "ghost":
-      document.getElementById("type").src = "/assets/pokemonTypes/Ghost.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Ghost.png";
       break;
     case "steel":
-      document.getElementById("type").src = "/assets/pokemonTypes/Steel.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Steel.png";
       break;
     case "fire":
-      document.getElementById("type").src = "/assets/pokemonTypes/Fire.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Fire.png";
       break;
     case "water":
-      document.getElementById("type").src = "/assets/pokemonTypes/Water.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Water.png";
       break;
     case "grass":
-      document.getElementById("type").src = "/assets/pokemonTypes/Grass.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Grass.png";
       break;
     case "electric":
-      document.getElementById("type").src = "/assets/pokemonTypes/Electric.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Electric.png";
       break;
     case "psychic":
-      document.getElementById("type").src = "/assets/pokemonTypes/Psychic.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Psychic.png";
       break;
     case "ice":
-      document.getElementById("type").src = "/assets/pokemonTypes/Ice.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Ice.png";
       break;
     case "dragon":
-      document.getElementById("type").src = "/assets/pokemonTypes/Dragon.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Dragon.png";
       break;
     case "dark":
-      document.getElementById("type").src = "/assets/pokemonTypes/Dark.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Dark.png";
       break;
     case "fairy":
-      document.getElementById("type").src = "/assets/pokemonTypes/Fairy.png";
+      document.getElementById("type").src = "public/assets/pokemonTypes/Fairy.png";
       break;
     default:
       break;
@@ -362,60 +365,60 @@ function Types1() {
 function Types2() {
   switch (SecondType) {
     case "normal":
-      document.getElementById("type2").src = "/assets/pokemontypes/Normal.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Normal.png";
       break;
     case "fighting":
       document.getElementById("type2").src =
-        "/assets/pokemontypes/Fighting.png";
+        "public/assets/pokemontypes/Fighting.png";
       break;
     case "flying":
-      document.getElementById("type2").src = "/assets/pokemontypes/Flying.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Flying.png";
       break;
     case "poison":
-      document.getElementById("type2").src = "/assets/pokemontypes/Poison.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Poison.png";
       break;
     case "ground":
-      document.getElementById("type2").src = "/assets/pokemontypes/Ground.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Ground.png";
       break;
     case "rock":
-      document.getElementById("type2").src = "/assets/pokemontypes/Rock.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Rock.png";
       break;
     case "bug":
-      document.getElementById("type2").src = "/assets/pokemontypes/Bug.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Bug.png";
       break;
     case "ghost":
-      document.getElementById("type2").src = "/assets/pokemontypes/Ghost.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Ghost.png";
       break;
     case "steel":
-      document.getElementById("type2").src = "/assets/pokemontypes/Steel.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Steel.png";
       break;
     case "fire":
-      document.getElementById("type2").src = "/assets/pokemontypes/Fire.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Fire.png";
       break;
     case "water":
-      document.getElementById("type2").src = "/assets/pokemontypes/Water.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Water.png";
       break;
     case "grass":
-      document.getElementById("type2").src = "/assets/pokemontypes/Grass.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Grass.png";
       break;
     case "electric":
       document.getElementById("type2").src =
-        "/assets/pokemontypes/Electric.png";
+        "public/assets/pokemontypes/Electric.png";
       break;
     case "psychic":
-      document.getElementById("type2").src = "/assets/pokemontypes/Psychic.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Psychic.png";
       break;
     case "ice":
-      document.getElementById("type2").src = "/assets/pokemontypes/Ice.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Ice.png";
       break;
     case "dragon":
-      document.getElementById("type2").src = "/assets/pokemontypes/Dragon.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Dragon.png";
       break;
     case "dark":
-      document.getElementById("type2").src = "/assets/pokemontypes/Dark.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Dark.png";
       break;
     case "fairy":
-      document.getElementById("type2").src = "/assets/pokemontypes/Fairy.png";
+      document.getElementById("type2").src = "public/assets/pokemontypes/Fairy.png";
       break;
     case "":
       Type2.hidden = true;
@@ -506,7 +509,7 @@ const EvolutionChain = async () => {
               const data = await promise.json();
               let familyName = document.createElement("img");
               if(data.sprites.other.home.front_default === null){
-                familyName.src = "/assets/pokeball-pokemon-catch.svg"
+                familyName.src = "public/assets/pokeball-pokemon-catch.svg"
               } else {
                 familyName.src = data.sprites.other.home.front_default;
               }
@@ -619,8 +622,10 @@ const getPokemon = async () => {
     }
   };
    function PokemonAudio() {
-    document.getElementById("pokemonCry").src = data.cries.latest;
-    PokemonCry.play();
+    if(PokemonAudioBool === true){
+      document.getElementById("pokemonCry").src = data.cries.latest;
+      PokemonCry.play();
+    }
   }
   function MoveFunction(){
     for (let i = 0; i < data.moves.length; i++) {
@@ -670,7 +675,7 @@ function SearchFunction() {
   SpecificSearches()
   Default = "";
   Shiny = "";
-  document.getElementById("shinyIcon").src = "/assets/Shiny.png";
+  document.getElementById("shinyIcon").src = "public/assets/Shiny.png";
   ShinyImg = true;
   empty(list);
   empty(Family);
@@ -703,20 +708,20 @@ function ShinyFunction() {
   ShinyBtn.addEventListener("click", async () => {
     if (ShinyImg === true) {
       if (Name.innerText === "#000 - MissingNo.") {
-        document.getElementById("pokemonImg").src = "/assets/Ketsuban.png";
+        document.getElementById("pokemonImg").src = "public/assets/Ketsuban.png";
       } else {
         document.getElementById("pokemonImg").src = Shiny;
       }
       ShinyAudio.play();
-      document.getElementById("shinyIcon").src = "/assets/ShinyActive.png";
+      document.getElementById("shinyIcon").src = "public/assets/ShinyActive.png";
       ShinyImg = false;
     } else {
       if (Name.innerText === "#000 - MissingNo.") {
-        document.getElementById("pokemonImg").src = "/assets/Missingno_RB.png";
+        document.getElementById("pokemonImg").src = "public/assets/Missingno_RB.png";
       } else {
         document.getElementById("pokemonImg").src = Default;
       }
-      document.getElementById("shinyIcon").src = "/assets/Shiny.png";
+      document.getElementById("shinyIcon").src = "public/assets/Shiny.png";
       ShinyImg = true;
     }
   });
@@ -729,7 +734,7 @@ RandomBtn.addEventListener("click", async () => {
   FetchLink = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
   Default = "";
   Shiny = "";
-  document.getElementById("shinyIcon").src = "/assets/Shiny.png";
+  document.getElementById("shinyIcon").src = "public/assets/Shiny.png";
   ShinyImg = true;
   getPokemon();
   empty(list);
@@ -745,7 +750,7 @@ function RandomPokemon() {
 
 function MissingNoInfo() {
   Name.innerText = "#000 - MissingNo.";
-  document.getElementById("pokemonImg").src = "/assets/Missingno_RB.png";
+  document.getElementById("pokemonImg").src = "public/assets/Missingno_RB.png";
   HP.innerText = "HP: 33";
   Attack.innerText = "Attack: 136";
   Defense.innerText = "Defense: 0";
@@ -756,7 +761,7 @@ function MissingNoInfo() {
   Ability2.hidden = true;
   Ability3.hidden = true;
   LocationInfo.innerText = "N/A";
-  document.getElementById("type").src = "/assets/pokemonTypes/Normal.png";
+  document.getElementById("type").src = "public/assets/pokemonTypes/Normal.png";
   Type2.hidden = true;
   CurrentMon = "0";
 
@@ -770,7 +775,7 @@ function MissingNoInfo() {
   let LocationNum = Math.floor(Math.random() * MissingNoLocation.length);
   LocationInfo.innerText = MissingNoLocation[LocationNum];
 
-  document.getElementById("pokemonCry").src = "/assets/audio/MissingNoCry.wav";
+  document.getElementById("pokemonCry").src = "public/assets/audio/MissingNoCry.wav";
   PokemonCry.play();
 
   for (let i = 0; i < MissingNoFamily.length; i++) {
@@ -784,20 +789,20 @@ function MissingNoInfo() {
 
 function IsSaved() {
   if (getFromLocalStorage().includes(CurrentMon)) {
-    document.getElementById("savedPokemon").src = "/assets/2Active.svg";
+    document.getElementById("savedPokemon").src = "public/assets/2Active.svg";
   } else {
-    document.getElementById("savedPokemon").src = "/assets/2.svg";
+    document.getElementById("savedPokemon").src = "public/assets/2.svg";
   }
 }
 
 SaveBtn.addEventListener("click", async () => {
   if (getFromLocalStorage().includes(CurrentMon)) {
-    document.getElementById("savedPokemon").src = "/assets/2.svg";
+    document.getElementById("savedPokemon").src = "public/assets/2.svg";
     SaveBool = true
     removeFromLocalStorage(CurrentMon);
     createElements();
   } else {
-    document.getElementById("savedPokemon").src = "/assets/2Active.svg";
+    document.getElementById("savedPokemon").src = "public/assets/2Active.svg";
     SaveBool = false
     saveToLocalStorage(CurrentMon);
     createElements();
@@ -847,7 +852,7 @@ function createElements() {
         EvolutionLink = `https://pokeapi.co/api/v2/pokemon-species/${userInput}`;
         Default = "";
         Shiny = "";
-        document.getElementById("shinyIcon").src = "/assets/Shiny.png";
+        document.getElementById("shinyIcon").src = "public/assets/Shiny.png";
         ShinyImg = true;
         empty(list);
         empty(Family);
